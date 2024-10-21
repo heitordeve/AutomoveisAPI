add7dcaf8449eb20a1a1e3a386797a57622bfae0
heitordeve <heitor.developer@outlook.com>
2024-10-21 16:37:29 -0300

    Criando modelo de automovel

--- Models/Automovel.cs
+++ Models/Automovel.cs
@@ -1,1 +1,9 @@
-namespace AutomoveisAPI.Models
+namespace AutomoveisAPI.Models{
+    public class Automovel{
+        public int Id {get; set;}
+        public string Marca {get; set;}
+        public string Modelo {get; set;}
+        public int Ano {get; set;}
+        public decimal Preco {get; set;}
+    }
+}
